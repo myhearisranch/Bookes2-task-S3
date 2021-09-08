5647cf6f5b654d2aec6d728b5292fa63d7f762e1
myhearisranch <myhearisranch@gmail.com>
2021-09-08 07:34:11 +0000

    編集機能を付けた

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,7 @@ Rails.application.routes.draw do
   get 'home/about' => 'homes#about'
 
   devise_for :users
-  resources :books, only: [ :create, :index, :show, :destroy, :edit]
+  resources :books, only: [ :create, :index, :show, :destroy, :edit , :update]
   patch 'books/:id' => 'books#create'
   resources :users, only: [:show, :edit, :update, :index]
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
